b5f07443948d12de2b04fbed4445cb60c41935ac
Megavexus <nexus.megavexus@gmail.com>
2016-11-06 12:52:47 +0000

    cleaned test files

--- test2.py
+++
@@ -1,9 +0,0 @@
-import math
-
-alfa = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-for i in range(len(alfa)):
-    for j in range(len(alfa[i::]+alfa[i::])):
-        print alfa[j]
-        
-    print '\n'
